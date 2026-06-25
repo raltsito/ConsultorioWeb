4689d45896ebf88ac2ef448cdd292b8360c0a7c3
RocioMedC <rocio.medina9346@gmail.com>
2026-06-24 19:27:11 -0600

    Revertir cambio de templates para restaurar servicio

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'clinica' / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
